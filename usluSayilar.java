f7f6496ed80e80843876977f40f880d90e0586b0
Cuma YILDIZ <142337560+cumaayildizz@users.noreply.github.com>
2023-09-16 14:59:17 +0300

    Add files via upload

---
+++ usluSayilar.java
@@ -0,0 +1,48 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class usluSayilar {
+    public static void main(String[] args) {
+
+        int n,r,total=1;
+        //n ve r değerlerini kullanıcıdan alacağız.Sonuca da bir değer atayıp tanımsız olmasını engelledik.
+        Scanner input=new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("****GİRİLEN SAYININ ÜSSÜNÜ ALAN PROGRAM****");
+        System.out.println();
+
+        System.out.print("Üssü alınacak sayı: ");
+        n=input.nextInt();
+
+        System.out.println();
+
+        System.out.print("Üs olacak sayı: ");
+        r=input.nextInt();
+
+        if(n>=0 && r>=0 ){
+
+            for(int i=1; i<=r; i++){
+
+                total*=n;
+
+            }
+            System.out.println();
+            System.out.print("Sonuç= " + total);
+
+        }
+
+        else if (n==0 && r==0){
+            System.out.println(" 0^0 işlemi tanımsız");
+        }
+
+        else {
+
+            System.out.println("LÜTFEN POZİTİF TAM SAYI GİRİNİZ!!");
+
+        }
+
+
+    }
+}
